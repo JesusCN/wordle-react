bfaad9189658baa0c823023572a14d00200e16c5
jesus c <jesus.cruz@mail.com>
2022-06-29 11:02:15 -0500

    feat: add word rows to board

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -1,15 +1,26 @@
 import "./Board.css";
 import BoardRow from "./BoardRow";
+import Proptypes from "prop-types";
 
-function Board() {
+Board.propTypes = {
+  words: Proptypes.array,
+  maxWords: Proptypes.number,
+  maxLetters: Proptypes.number,
+};
+
+function Board({ words = [], maxWords = 6, maxLetters = 5 }) {
+  
+  const wordList = [...words, ...Array(maxWords - words.length).fill("")];
+
   return (
     <div className="board">
-      <BoardRow word={"adios"}></BoardRow>
-      <BoardRow word={"adios"}></BoardRow>
-      <BoardRow word={"adios"}></BoardRow>
-      <BoardRow word={"adios"}></BoardRow>
-      <BoardRow word={"adios"}></BoardRow>
-      <BoardRow word={"adios"}></BoardRow>
+      {wordList.map((w, i) => (
+        <BoardRow
+          key={"word_row_" + i}
+          word={w}
+          maxLetters={maxLetters}
+        ></BoardRow>
+      ))}
     </div>
   );
 }
